262b6f309d5f5a709a465914ae7589ebc9c52882
Daniel Breves <dbrevesf@gmail.com>
2018-09-13 11:54:03 -0300

    Create CallDetail model

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Call
+from .models import Call, CallDetail
 
 # Register your models here.
 admin.site.register(Call)
+admin.site.register(CallDetail)
--- api/models.py
+++ api/models.py
@@ -14,8 +14,26 @@ class Call(models.Model):
         destination (models.CharField): telephone number of the destination
 
     """
-    source = models.CharField(max_length=11, required=True)
-    destination = models.CharField(max_length=11, required=True)
+    source = models.CharField(max_length=11)
+    destination = models.CharField(max_length=11)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class CallDetail(models.Model):
+    """
+    Database model for the details of a telephone call.telephone
+
+    Attributes:
+        call_id (Call.id): the foreign key from Call
+        start (models.BooleanField): start (True) or end (False) of a call
+        timestamp (models.DateTimeField): when the call was started or finished
+
+    """
+    call_id = models.ForeignKey('Call', on_delete=models.CASCADE)
+    start = models.BooleanField(default=True)
+    timestamp = models.DateTimeField()
 
     def __str__(self):
         return str(self.id)
