d30879965a45b1a3a85ed06fffad65445ddb1c16
Daniel Breves <dbrevesf@gmail.com>
2018-09-13 11:47:31 -0300

    Create Call model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
+"""
+Model which contains all of the models used by our application
+"""
+
 from django.db import models
 
-# Create your models here.
+
+class Call(models.Model):
+    """
+    Database model for the telephone calls.
+
+    Attributes:
+        source (models.CharField): telephone number of the source
+        destination (models.CharField): telephone number of the destination
+
+    """
+    source = models.CharField(max_length=11, required=True)
+    destination = models.CharField(max_length=11, required=True)
+
+    def __str__(self):
+        return str(self.id)
