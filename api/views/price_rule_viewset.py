b3963b719a99bee25e22dd25aa432d45ca79d664
Daniel Breves <dbrevesf@gmail.com>
2018-09-18 11:10:45 -0300

    Adjust PriceRuleViewSet

--- api/views/price_rule_viewset.py
+++ api/views/price_rule_viewset.py
@@ -1,8 +1,6 @@
 from api.models import PriceRule
 from api.serializers import PriceRuleSerializer
 from rest_framework import viewsets
-from rest_framework import status
-from rest_framework.response import Response
 
 
 class PriceRuleViewSet(viewsets.ModelViewSet):
