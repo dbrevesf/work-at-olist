c2c75f07b3ddbfa3ba56f191baa2441def7d7db2
Daniel Breves <dbrevesf@gmail.com>
2018-09-18 15:22:15 -0300

    Refact standardize_date in utils.py

--- api/utils.py
+++ api/utils.py
@@ -4,24 +4,23 @@ Module which contains several usefull methods to be used in this project
 from datetime import datetime
 
 
-def standardize_date(date):
+def standardize_date(date, pattern):
     """
     Method that standards a date object.
 
     Parameters:
         date (String/datetime): date object in string or datetime object
+        pattern (string): the pattern that the time must match
 
     Return:
         date_datetime (Datetime.datetime): Date in a datetime standard
         format.
     """
-    pattern = '%Y-%m-%dT%H:%M:%SZ'
     if isinstance(date, datetime):
-        date = date.strftime('%Y-%m-%dT%H:%M:%SZ')
+        date = date.strftime(pattern)
     try:
         standard_date = datetime.strptime(date, pattern)
     except ValueError:
-        standard_date = {'input_error':
-                         'Time date must match format YYYY-MM-DDTHH:MM:SSZ'}
-
+        error_msg = ('time date must match format %s' % (pattern))
+        standard_date = {'input_error': error_msg}
     return standard_date
