96b318e918034435d958068f760fb068741a154a
Daniel Breves <dbrevesf@gmail.com>
2018-09-18 10:03:05 -0300

    Adjust stardardize_date

--- api/utils.py
+++ api/utils.py
@@ -18,6 +18,10 @@ def standardize_date(date):
     pattern = '%Y-%m-%dT%H:%M:%SZ'
     if isinstance(date, datetime):
         date = date.strftime('%Y-%m-%dT%H:%M:%SZ')
-    standard_date = datetime.strptime(date, pattern)
+    try:
+        standard_date = datetime.strptime(date, pattern)
+    except ValueError:
+        standard_date = {'input_error':
+                         'Time date must match format YYYY-MM-DDTHH:MM:SSZ'}
 
     return standard_date
